a6256ad9e37e0040d5a128ecbfb9325ec7cdd5cb
ghidella <raphaelfghidella@gmail.com>
2023-02-22 20:26:34 -0300

    excluding .git on the rust folders

---
+++ guessing_game/target/debug/guessing_game.d
@@ -0,0 +1,1 @@
+A:\2023\rust\guessing_game\target\debug\guessing_game.exe: A:\2023\rust\guessing_game\src\main.rs
---
+++ hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+A:\2023\rust\hello_cargo\target\debug\hello_cargo.exe: A:\2023\rust\hello_cargo\src\main.rs
