8e77742dd38c1d3264add8b98c605317f34d878c
ghidella <raphaelfghidella@gmail.com>
2023-02-26 18:49:56 -0300

    variables and mutability

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let x = 5;
-    println!("The value of x is: {x}");
-    let mut y = x;
-    y = y + 1;
-    println!("The value of x is: {y}");
+    let  x = 5;
+
+    let x = x + 1;
+    
+    {
+        let x = x * 2;
+        println!("The value of x in the inner scope is: {x}");
+    }
+    
+    println!("The value of x is: {x}"); 
 }
---
+++ variables/target/debug/deps/variables-bd6eda43aa1ac505.d
@@ -0,0 +1,5 @@
+a:\2023\rust\variables\target\debug\deps\variables-bd6eda43aa1ac505.rmeta: src\main.rs
+
+a:\2023\rust\variables\target\debug\deps\variables-bd6eda43aa1ac505.d: src\main.rs
+
+src\main.rs:
