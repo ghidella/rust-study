7dcf77d2aa8629318c1aa1ffebf5ea705104339c
Ghidella <79724280+ghidella@users.noreply.github.com>
2023-02-26 18:55:52 -0300

    Delete variables/target directory

--- variables/target/debug/deps/variables-f62e754f7de194e5.d
+++
@@ -1,5 +0,0 @@
-a:\2023\rust\variables\target\debug\deps\variables-f62e754f7de194e5.rmeta: src\main.rs
-
-a:\2023\rust\variables\target\debug\deps\variables-f62e754f7de194e5.d: src\main.rs
-
-src\main.rs:
--- variables/target/debug/variables.d
+++
@@ -1,1 +0,0 @@
-A:\2023\rust\variables\target\debug\variables.exe: A:\2023\rust\variables\src\main.rs
