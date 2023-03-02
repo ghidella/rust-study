8052c9095ba4f1a534b98f97a4f648c6fbf2ff98
ghidella <raphaelfghidella@gmail.com>
2023-03-02 20:59:19 -0300

    using structs and some debug

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -19,7 +19,21 @@ fn main() {
     //Debug. That enable us to print our struct in a way that is useful
     //to see its value while we're debugging our code
     //we need to add the outer attribute #[derive(Debug)] before defining the struct
-    
+    println!("rect1 is {:#?}", rect1);
+
+    //another way to print out a value using Debug is the dbg! macro
+    //it takes the ownership of an expression(as opposed to println!)
+    //prints the file and line where the call occurs and returns ownership 
+    dbg!(&rect1);
+
+    let scale = 2;
+    let rect2 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect2);
+
     println!(
         "The area of the rectangle is {} square pixels.",
         area(&rect1)
